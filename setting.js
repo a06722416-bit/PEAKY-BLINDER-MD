4f8e90494a7817de713e7da71fa86a03c3a6f46d
a06722416-bit <a06722416@gmail.com>
2026-05-13 03:41:31 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -6,7 +6,7 @@ module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'ᴘᴇᴀᴋʏ-ʙʟɪɴᴅᴇʀ-ᴍᴅ', 
   OWNER_NAME: process.env.OWNER_NAME || 'Mr Lee', 
   OWNER_NUMBER: process.env.OWNER_NUMBER || '254116266407', 
-  SESSION_ID: process.env.SESSION_ID || '', // paste here your blinders session ID
+  SESSION_ID: process.env.SESSION_ID || '', // blinder~aqBQQKSB#JdhUBLO_4bMxDGTCIvsg2hnIJVMpq9IohU8uOmUCoO8 
   MODE: process.env.MODE || "private", // public or private
   TIMEZONE: process.env.TIMEZONE || 'Africa/Nairobi' 
 };
